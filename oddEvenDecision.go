a48ed2062d355291e208f239fb57fc18fec6b7af
narwhal04 <q.quek7@gmail.com>
2022-03-31 14:00:40 +0800

    working code

--- oddEvenDecision.go
+++ oddEvenDecision.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
+var userInput int
+
 func main() {
-	//Insert code here
+	fmt.Println("Please Input a number")
+	fmt.Scan(&userInput)
+
+	if userInput == 0 {
+		fmt.Println(userInput, "is an even number")
+	} else if userInput%2 == 0 {
+		fmt.Println(userInput, "is an even number")
+	} else {
+		fmt.Println(userInput, "is an odd number")
+	}
 }
